2c37e11fae5e1fe1f879f1bf6695881242f6a7e0
Damian Roiz <damianx989@gmail.com>
2023-04-20 09:32:30 -0400

    removed favicon request to server

--- course/course.js
+++ course/course.js
@@ -1,9 +1,9 @@
 // var, const and let declarations
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthdate = 1911;
+// const birthdate = 1911;
 // const cannoot be empty variables
 // cosnt job; is not allowed
 
@@ -17,37 +17,54 @@ const birthdate = 1911;
 
 //operator precedence
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(now - 1991 > now - 2018)
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(now - 1991 > now - 2018)
 
-let x, y;
-x = y = 25 - 10 - 5;
-console.log(x, y)
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y)
 
-const averageAge = (ageJonas + ageSarah) / 2 
-console.log(ageJonas, ageSarah, averageAge)
+// const averageAge = (ageJonas + ageSarah) / 2
+// console.log(ageJonas, ageSarah, averageAge)
 
 ////////////////////////////////
 //Coding Challenge #1
 
-//1
-const markWeight = 78 
-const markHeight = 1.69 
-const johnWeight = 92
-const johnHeight = 1.95 
+// //1
+// const markWeight = 78
+// const markHeight = 1.69
+// const johnWeight = 92
+// const johnHeight = 1.95
 
-//2
-const markBMI = markWeight / markHeight ** 2
-console.log(markBMI);
+// //2
+// const markBMI = (markWeight / markHeight ** 2).toFixed(2)
 
-const johnBMI = johnWeight / johnHeight ** 2 
-console.log(johnBMI)
+// console.log(markBMI);
 
-//3
-const markHeigherBMI = markBMI > johnBMI;
-console.log(markHeigherBMI)
+// const johnBMI = (johnWeight / johnHeight ** 2).toFixed(2)
+// console.log(johnBMI)
 
+// //3
+// const markHeigherBMI = markBMI > johnBMI;
+// console.log(markHeigherBMI)
 
+// Template literals
 
+// const firstName = "Jonas"
+// const job = "Teacher"
+// const birthdate = 1991
+// const year = 2037
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthdate} year old ${job}`
+// console.log(jonasNew);
+
+//Taking decision if / else statements
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log("Sarah can start driving license");
+}
